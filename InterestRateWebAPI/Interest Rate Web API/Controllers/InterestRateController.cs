eb824c40a5fea2d511d9c689c5985f60fecb1799
ozanp <ozanpembegul@gmail.com>
2022-06-26 20:58:30 +0300

    PaymentPlan

--- InterestRateWebAPI/Interest Rate Web API/Controllers/InterestRateController.cs
+++ InterestRateWebAPI/Interest Rate Web API/Controllers/InterestRateController.cs
@@ -10,8 +10,8 @@ namespace Interest_Rate_Web_API.Controllers
     public class InterestRateController : Controller
     {
 
-        [HttpGet]
-        public List<float> GetInterestRate(string LoanType, double LoanAmount, int Delay)
+        [HttpGet("TotalRepayment")]
+        public List<float> GetTotalRepayment(string LoanType, double LoanAmount, int Delay)
         {
             float InterestRate;
             double MonthlyPayment;
@@ -45,6 +45,59 @@ namespace Interest_Rate_Web_API.Controllers
 
 
             MonthlyPayment = LoanAmount * Math.Pow((1 + InterestRate), Delay) * InterestRate / (Math.Pow((1 + InterestRate), Delay) - 1);
+            RepaymentAmount = (float)MonthlyPayment * Delay;
+            TotalInterest = (float)RepaymentAmount - (float)LoanAmount;
+
+
+            List<float> Result = new List<float>()
+            {
+
+                TotalInterest, RepaymentAmount
+
+            };
+
+            return Result;
+
+        }
+
+        [HttpGet("PaymentPlan")]
+        public List<float> GetPaymentPlan(string LoanType, double LoanAmount, int Delay)
+        {
+            float InterestRate;
+            double MonthlyPayment;
+            float RepaymentAmount = 0f;
+            float TotalInterest = 0f;            
+
+
+            if (LoanType == "sme") // esnaf kredisi
+            {
+                InterestRate = 0.0123f;
+            }
+            else if (LoanType == "housing")
+            {
+                InterestRate = 0.0099f;
+            }
+
+            else if (LoanType == "vehicle")
+            {
+                InterestRate = 0.0165f;
+            }
+
+            else if (LoanType == "student")
+            {
+                InterestRate = 0.0135f;
+            }
+
+            else
+            {
+                InterestRate = 0.02f;
+            }
+
+
+            MonthlyPayment = LoanAmount * Math.Pow((1 + InterestRate), Delay) * InterestRate / (Math.Pow((1 + InterestRate), Delay) - 1);
+
+
+
             RepaymentAmount = (float)MonthlyPayment * Delay;
             TotalInterest = (float)RepaymentAmount - (float)LoanAmount;
 
